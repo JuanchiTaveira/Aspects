354f2c6b487c515eee2736dfe705362394c1df23
Juan Taveira <juantaveira10@gmail.com>
2024-06-26 20:52:37 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./styles/main.scss";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-500">¡Hola mundo!</h1>
+    </>
+  )
+}
+
+export default App;
