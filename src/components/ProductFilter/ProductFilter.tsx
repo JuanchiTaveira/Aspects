57c6354d67339bf0c15c847c8ef0a7e7942e789f
Juan Taveira <juantaveira10@gmail.com>
2024-07-13 19:46:45 +0200

    ProductFilters sticky

--- src/components/ProductFilter/ProductFilter.tsx
+++ src/components/ProductFilter/ProductFilter.tsx
@@ -2,7 +2,7 @@ import styles from './ProductFilter.module.scss';
 
 export const ProductFilter = () => {
     return (
-        <div className={styles.filters}>
+        <div className={`sticky ${styles.filters}`}>
             <div className={`flex justify-between items-center border-b`}>
                 <button className="text-sm px-4 py-4">Shop All</button>
                 <button className="text-sm px-4 py-4">Refine</button>
