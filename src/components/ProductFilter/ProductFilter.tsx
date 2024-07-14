b8562fd13a12b742a46bc8d20826b1a5a5f991c4
Juan Taveira <juantaveira10@gmail.com>
2024-07-14 17:24:29 +0200

    productFilters without animations

--- src/components/ProductFilter/ProductFilter.tsx
+++ src/components/ProductFilter/ProductFilter.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react';
 import styles from './ProductFilter.module.scss';
 
 export const ProductFilter = () => {
+
+    const [isVisible, setIsVisible] = useState(true);
+    
+    useEffect(() => {
+        const mainElement = document.querySelector("main");
+        let lastScrollY = mainElement!.scrollTop;
+
+        const handleScroll = () => {            
+            const currentScrollY = mainElement!.scrollTop;
+            const shouldBecomeVisible = currentScrollY < lastScrollY;
+
+            // Solo actualiza el estado si el valor ha cambiado
+            if (shouldBecomeVisible !== isVisible) {
+                setIsVisible(shouldBecomeVisible);
+            }
+            
+            lastScrollY = currentScrollY;
+        };
+
+        mainElement!.addEventListener("scroll", handleScroll);
+
+        return () => {
+            mainElement!.removeEventListener("scroll", handleScroll);
+        };
+    }, [isVisible]);
+
     return (
-        <div className={`sticky ${styles.filters}`}>
+        <div className={`sticky top-0 z-[1] ${styles.filters} ${!isVisible ? "hidden" : ""}`}>
             <div className={`flex justify-between items-center border-b`}>
                 <button className="text-sm px-4 py-4">Shop All</button>
                 <button className="text-sm px-4 py-4">Refine</button>
