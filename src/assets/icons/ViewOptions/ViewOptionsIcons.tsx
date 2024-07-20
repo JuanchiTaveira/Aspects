235e7beba6ce6f87084bc4db17f3f18db5e3e994
Juan Taveira <juantaveira10@gmail.com>
2024-07-20 20:54:41 +0200

    extract ViewOptionsIcons

---
+++ src/assets/icons/ViewOptions/ViewOptionsIcons.tsx
@@ -0,0 +1,58 @@
+
+export const ViewOptionsX8 = () => {
+    return (
+        <svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="pl-2">
+            <rect width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="18" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="27" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="36" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="45" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="54" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="63" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="9" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="18" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="27" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="36" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="45" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="54" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="63" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+        </svg>
+    )
+}
+
+export const ViewOptionsX4 = () => {
+    return (
+        <svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="pl-2">
+            <rect width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="18" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="27" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="9" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="18" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="27" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+        </svg>
+    )
+}
+
+export const ViewOptionsX2 = () => {
+    return (
+        <svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="pl-2">
+            <rect width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect y="9" width="7" height="7" fill="#C4C4C4"></rect>
+            <rect x="9" y="9" width="7" height="7" fill="#C4C4C4"></rect>
+        </svg>
+    )
+}
+
+export const ViewOptionsX1 = () => {
+    return (
+        <svg width="7" height="16" viewBox="0 0 7 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="pl-2">
+            <rect width="7" height="7" fill="#C4C4C4"></rect>
+            <rect y="9" width="7" height="7" fill="#C4C4C4"></rect>
+        </svg>
+    )
+}
--- src/components/ViewOptions/ViewOptions.tsx
+++ src/components/ViewOptions/ViewOptions.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import styles from '../OffcanvasFilters/OffcanvasFilters.module.scss'; //importing scss file for OffcanvasFilters, as the styles are similar
 import RoundedBlackCheckbox from '../RoundedBlackCheckbox/RoundedBlackCheckbox';
+import { ViewOptionsX8, ViewOptionsX4, ViewOptionsX2 } from '../../assets/icons/ViewOptions/ViewOptionsIcons';
 
 export const ViewOptions = () => {
 
@@ -36,46 +37,15 @@ export const ViewOptions = () => {
                     <div className={`list-group ${styles.listGroup} flex flex-row justify-between`}>
                         <div className="flex flex-row">
                             <RoundedBlackCheckbox id="x8" type="radio" value="8" name="viewOptionRadio" handleClick={handleRadioChange} />
-                            <svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="pl-2">
-                                <rect width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="18" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="27" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="36" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="45" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="54" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="63" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="9" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="18" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="27" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="36" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="45" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="54" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="63" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                            </svg>
+                            <ViewOptionsX8 />
                         </div>
                         <div className="flex flex-row pl-3">
                             <RoundedBlackCheckbox id="x4" type="radio" value="4" name="viewOptionRadio" handleClick={handleRadioChange} />
-                            <svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="pl-2">
-                                <rect width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="18" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="27" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="9" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="18" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="27" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                            </svg>
+                            <ViewOptionsX4 />
                         </div>
                         <div className="flex flex-row pl-3">
                             <RoundedBlackCheckbox id="x2" type="radio" value="2" name="viewOptionRadio" handleClick={handleRadioChange} />
-                            <svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="pl-2">
-                                <rect width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                                <rect x="9" y="9" width="7" height="7" fill="#C4C4C4"></rect>
-                            </svg>
+                            <ViewOptionsX2 />
                         </div>
                     </div>
                 </div>
