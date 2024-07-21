e44a387dd69718f15bee1df9c75bf861db9e3bde
Juan Taveira <juantaveira10@gmail.com>
2024-07-21 18:42:55 +0200

    change menuIcon color to white when search active

--- src/components/MenuIcon/MenuIcon.tsx
+++ src/components/MenuIcon/MenuIcon.tsx
@@ -1,10 +1,13 @@
 import styles from './MenuIcon.module.scss';
 import { SideMenu } from '../SideMenu/SideMenu'; // Import the SideMenu component
+import { useOverlay } from '../../context/OverlayContext';
 
 export const MenuIcon = () => {
+  const { isActive } = useOverlay();
+  
   return (
     <>
-      <button className="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+      <button className={`btn ${isActive ? styles.btnActive : ''}`} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-list w-[30px] h-[30px]" viewBox="0 0 16 16">
           <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
         </svg>
