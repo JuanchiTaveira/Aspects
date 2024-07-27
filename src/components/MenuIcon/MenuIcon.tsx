c96ca272cdbfdbbe8b76f950d4976f872790adb0
Juan Taveira <juantaveira10@gmail.com>
2024-07-27 13:00:03 +0200

    disable sidemenu button when active search

--- src/components/MenuIcon/MenuIcon.tsx
+++ src/components/MenuIcon/MenuIcon.tsx
@@ -3,11 +3,11 @@ import { SideMenu } from '../SideMenu/SideMenu';
 import { useSearchOverlay } from '../../context/SearchOverlayContext';
 
 export const MenuIcon = () => {
-  const { isActiveSearch: isActive } = useSearchOverlay();
+  const { isActiveSearch } = useSearchOverlay();
   
   return (
     <>
-      <button className={`btn ${isActive ? styles.btnActive : ''}`} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+      <button className={`btn ${isActiveSearch ? styles.btnActive : ''}`} disabled={isActiveSearch} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-list w-[30px] h-[30px]" viewBox="0 0 16 16">
           <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
         </svg>
