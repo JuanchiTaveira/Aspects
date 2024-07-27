88fcc7f7a2f3d25b1a0a931c29ca39f9b9b27d99
Juan Taveira <juantaveira10@gmail.com>
2024-07-27 15:25:49 +0200

    add side menu footer

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -2,11 +2,33 @@ import styles from './SideMenu.module.scss';
 
 export const SideMenu = () => {
     return (
-        <div className={`offcanvas-body ${styles.offcanvasBody}`}>
-            <div className={styles.menuItem}>SHOP</div>
-            <div className={styles.menuItem}>COLLECTIONS</div>
-            <div className={styles.menuItem}>NEWS</div>
-            <div className={styles.menuItem}>ABOUT</div>
+        <div className={`offcanvas-body ${styles.offcanvasBody} flex flex-col justify-between`}>
+            <div className="navContent">
+                <ul>
+                    <li className={styles.menuItem}>SHOP</li>
+                    <li className={styles.menuItem}>COLLECTIONS</li>
+                    <li className={styles.menuItem}>LOOKBOOK</li>
+                    <li className={styles.menuItem}>MUSIC</li>
+                    <li className={styles.menuItem}>ABOUT</li>
+                </ul>
+            </div>
+            <div className={`${styles.footer}`}>
+                <ul>
+                    <li>Contact us:</li>
+                    <ul className="pl-4">
+                        <li className={styles.link}>
+                            <a href="https://www.instagram.com/manyaspects/?utm_source=ig_web_button_share_sheet" target="_blank">Instagram</a>
+                        </li>
+                        <li className={styles.link}>
+                            <a href="mailto:aspects@gmail.com" target="_blank" rel="noopener noreferrer">
+                                aspects@gmail.com
+                            </a>
+                        </li>
+                    </ul>
+                    <li className={`${styles.link} ${styles.pointer}`}>Privacy Policy</li>
+                    <li className="pt-3">© 2024 Aspects EU. All rights reserved.</li>
+                </ul>
+            </div>
         </div>
     )
 }
