3ae4b87e2b9b49b79b131869f5f7d5a91b9e7ab4
Juan Taveira <juantaveira10@gmail.com>
2024-07-27 14:28:08 +0200

    change close button in side menu

--- src/components/MenuIcon/MenuIcon.tsx
+++ src/components/MenuIcon/MenuIcon.tsx
@@ -2,18 +2,21 @@ import styles from './MenuIcon.module.scss';
 import { SideMenu } from '../SideMenu/SideMenu';
 import { useSearchOverlay } from '../../context/SearchOverlayContext';
 import sideMenuIcon from '../../assets/icons/sideMenuIcon.png';
+import closeButtonBlack from '../../assets/icons/closeButtonBlack.png';
 
 export const MenuIcon = () => {
   const { isActiveSearch } = useSearchOverlay();
-  
+
   return (
     <>
       <button className={`btn ${isActiveSearch ? styles.btnActive : ''}`} disabled={isActiveSearch} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
-        <img src={sideMenuIcon} alt="Open side menu" className={`h-[30px] ${isActiveSearch ? 'invert' : ''}`}/>
+        <img src={sideMenuIcon} alt="Open side menu" className={`h-[30px] ${isActiveSearch ? 'invert' : ''}`} />
       </button>
       <div className={`offcanvas offcanvas-start ${styles.sideMenu}`} data-bs-scroll="true" tabIndex={-1} id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-        <div className={`offcanvas-header ${styles.offcanvasHeader}`} data-bs-theme="dark">
-          <button type="button" className="btn-close shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <div className={`offcanvas-header ${styles.offcanvasHeader} flex flex-row justify-end`} data-bs-theme="dark">
+          <button type="button" className="shadow-none" data-bs-dismiss="offcanvas" aria-label="Close">
+            <img src={closeButtonBlack} alt="Close filters" className="h-8 invert" />
+          </button>
         </div>
         <SideMenu />
       </div>
