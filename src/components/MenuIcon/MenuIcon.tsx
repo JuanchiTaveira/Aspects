fc5c19580a6d4c2e19f82f8f9cd4a3eb0b840707
Juan Taveira <juantaveira10@gmail.com>
2024-07-27 13:37:23 +0200

    change side menu icon

--- src/components/MenuIcon/MenuIcon.tsx
+++ src/components/MenuIcon/MenuIcon.tsx
@@ -1,6 +1,7 @@
 import styles from './MenuIcon.module.scss';
 import { SideMenu } from '../SideMenu/SideMenu';
 import { useSearchOverlay } from '../../context/SearchOverlayContext';
+import sideMenuIcon from '../../assets/icons/sideMenuIcon.png';
 
 export const MenuIcon = () => {
   const { isActiveSearch } = useSearchOverlay();
@@ -8,9 +9,7 @@ export const MenuIcon = () => {
   return (
     <>
       <button className={`btn ${isActiveSearch ? styles.btnActive : ''}`} disabled={isActiveSearch} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-list w-[30px] h-[30px]" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
-        </svg>
+        <img src={sideMenuIcon} alt="Open side menu" className={`h-[30px] ${isActiveSearch ? 'invert' : ''}`}/>
       </button>
       <div className={`offcanvas offcanvas-start ${styles.sideMenu}`} data-bs-scroll="true" tabIndex={-1} id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
         <div className={`offcanvas-header ${styles.offcanvasHeader}`} data-bs-theme="dark">
